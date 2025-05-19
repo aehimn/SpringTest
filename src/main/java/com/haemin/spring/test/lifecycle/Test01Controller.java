298429bb0d995dda611a42158dde809bed24afed
김해민 <heaminkim1007@gmail.com>
2025-05-19 16:40:23 +0900

    [김해민] - lifecycle test 1번 문제

---
+++ src/main/java/com/haemin/spring/test/lifecycle/Test01Controller.java
@@ -0,0 +1,39 @@
+package com.haemin.spring.test.lifecycle;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("lifecycle/test01")
+public class Test01Controller {
+	
+	@ResponseBody
+	@RequestMapping("/1")
+	public String htmlResponse() {
+		return ""
+				+ "<html>\n"
+				+ "		<head>\n"
+				+ "			<title>String 출력하기</title>\n"
+				+ "		</head>\n"
+				+ "		<body>\n"
+				+ "			<h2>테스트 프로젝트 완성</h2>\n"
+				+ "			<h3>해당 프로젝트를 통해서 문제 풀이를 진행 합니다.</h3>\n"
+				+ "		</body>\n"
+				+ "</html>";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/2")
+	public Map<String, Integer> jsonResponse() {
+		Map<String, Integer> scoreMap = new HashMap<>();
+		scoreMap.put("국어", 80);
+		scoreMap.put("수학", 90);
+		scoreMap.put("영어",  85);
+		return scoreMap;
+	}
+
+}
