1846c6376160bf9928b070052c254f7eba5b3673
김해민 <heaminkim1007@gmail.com>
2025-06-10 18:45:48 +0900

    [김해민] - ajax 문제

---
+++ src/main/java/com/haemin/spring/test/ajax/repository/FavoriteRepository.java
@@ -0,0 +1,19 @@
+package com.haemin.spring.test.ajax.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.haemin.spring.test.ajax.domain.Favorite;
+
+@Mapper
+public interface FavoriteRepository {
+	
+	public List<Favorite> selectFavoriteList();
+	
+	public int insertFavorite(
+			@Param("name") String name
+			, @Param("url") String url);
+
+}
